1efbd28ade2f3d0ecfe1f6ab8d100c5327f52ec3
gtra389 <52122223+gtra389@users.noreply.github.com>
2019-07-03 21:34:55 +0800

    Update MB7139.py

--- MB7139.py
+++ MB7139.py
@@ -5,10 +5,11 @@
 # Product number: MB7139-100
 # Serial communication protocol: Pulse-width measurement by using GPIO
 # Purpose:
-# Detect water level every 1 hr (Unit in cm)
+# Detect water level every 60 sec (Unit in cm)
 # 
 
 
+# Including
 from statistics import median
 import time
 import RPi.GPIO as GPIO
@@ -18,15 +19,15 @@ from urllib.request import urlopen
 
 # Definition of variable
 id_No    = "9999"
-sampNum  = 8
+sampList = []
+sampNum  = 20
 settlementDep = -1.0 # Unit in cm
 GPIO_TRIGECHO =  15  # Define GPIO to use on RPi
-countNum2 = -1
+countNum = -1
+officalThreshold = 5.0 # Unit in cm
 
-# Use BCM GPIO references
-# instead of physical pin numbers
+# Use BCM GPIO references instead of physical pin numbers 
 GPIO.setmode(GPIO.BCM)
-
 # Set pins as output and input
 GPIO.setup(GPIO_TRIGECHO,GPIO.OUT)  # Initial state as output
 # Set trigger to False (Low)
@@ -59,26 +60,25 @@ def sampling():
     return distance
 
 def findMedian():
-    countNum1 = 0
-    samplingList = []
-    while (countNum1 <= sampNum):
+    countNum = 0
+    while (countNum <= sampNum):
         samplingList.append(float(sampling()))
-        medianVal = median(samplingList)
-        print("Count Number = ", countNum1)
-        countNum1 += 1
+    medianVal = median(samplingList)
     return medianVal
 
 def pipeLg():
     pLg = 0.0
     print("Start to measure the total length of pipeline......")
-    pLg = findMedian()
+    pLg = round(findMedian(),3)
     return pLg
 
 def waterLev(arg1):
     while True:
         print("Start measurement......")
-        wLev = arg1 - findMedian()
+        wLev = round((arg1 - sampling()),3)
         if (wLev > settlementDep):
+            if (settlementDep < wLev < 0.3):
+                wLev = 0.0 # To reduce the noise from the environment
             break
         else:
             print("Error in detecting the water level.")
@@ -88,26 +88,34 @@ def waterLev(arg1):
             
 def httpPOST(String0, String1, String2, String3):
     timeStamp = strftime("%Y%m%d%H%M%S")
-    url = 'http://ec2-54-175-179-28.compute-1.amazonaws.com/update_general.php?site=Mucha&time='+str(timeStamp)+'&weather=0&id='+ str(String0) + \
-          '&air=0&acceleration=0&cleavage=0&incline=0&field1='+str(String1)+'&field2='+str(String2)+'&field3='+str(String3)
-    resp = urlopen(url).read()
-    print(resp)
+    url = 'http://ec2-54-175-179-28.compute-1.amazonaws.com/update_general.php?site=Mucha&time='+repr(timeStamp)+'&weather=0&id='+ str(String0) + \
+          '&air=0&acceleration=0&cleavage=0&incline=0&field1='+repr(String1)+'&field2='+repr(String2)+'&field3='+repr(String3)
+    resp = urlopen(url).read()    
     print('------------------------')
+    
+def scenarioDetect(arg2):
+    if (arg2 > 0.0) & (arg2 < officalThreshold):        
+        sleepT = 30 # Unit in sec
+    elif (arg2 > 0.0) & (arg2 >= officalThreshold):
+        sleepT = 10 # Unit in sec
+    else:        
+        sleepT = 60 # Unit in sec        
+    return sleepT
     
 
 try:
     while True:
-        while (countNum2 < 0):
+        while (countNum < 0):
             pLgVal  = pipeLg()
             httpPOST(id_No, pLgVal, "Reboot", 0)
-            countNum2 +=1
+            countNum += 1
         
-        wLevVal = waterLev(pLgVal)              
-        print("Pipe length : %.2f cm" % pLgVal)
-        print("Water level : %.2f cm" % wLevVal)
-        httpPOST(id_No, wLevVal, 0, 0)
-        
-        time.sleep(30)
+        wLevVal = waterLev(pLgVal)
+        sleepTime = scenarioDetect(wLevVal)
+        print("Pipe length : %.3f cm" % pLgVal)
+        print("Water level : %.3f cm" % wLevVal)
+        httpPOST(id_No, wLevVal, 0, 0)        
+        time.sleep(sleepTime)
 
 except KeyboardInterrupt:
     print("Stop")
