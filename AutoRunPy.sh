752686cb20c8aaa72411658b8c52ae9983974a84
gtra389 <52122223+gtra389@users.noreply.github.com>
2019-07-07 00:27:25 +0800

    Update AutoRunPy.sh

--- AutoRunPy.sh
+++ AutoRunPy.sh
@@ -2,12 +2,12 @@
 
 sleep 15
 
-sudo python3 /home/pi/3meters/3m_main.py >> log
+sudo python /home/pi/FloDect/MB7139.py >> log
 while :
 do
   if [ `ps -U root -u root u | grep python3 | wc -m` -eq 0 ]
   then 
-    sudo python3 /home/pi/3meters/3m_main.py >> log
+    sudo python /home/pi/FloDect/MB7139.py >> log
     sleep 180
   else
     sleep 180
