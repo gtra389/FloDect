ce57238f9ccde34f69813cd856e82ed879640589
gtra389 <52122223+gtra389@users.noreply.github.com>
2019-07-07 17:56:42 +0800

    Update MB7139.py

--- MB7139.py
+++ MB7139.py
@@ -93,10 +93,16 @@ def httpPOST(String0, String1, String2, String3):
     try:
         global timeStamp
         timeStamp = strftime("%Y%m%d%H%M%S")
-        url = 'http://ec2-54-175-179-28.compute-1.amazonaws.com/update_general.php?site=Mucha&time='+repr(timeStamp)+'&weather=0&id='+ str(String0) + \
-              '&air=0&acceleration=0&cleavage=0&incline=0&field1='+repr(String1)+'&field2='+repr(String2)+'&field3='+repr(String3)
+        #url = 'http://ec2-54-175-179-28.compute-1.amazonaws.com/update_general.php?site=Mucha&time='+repr(timeStamp)+'&weather=0&id='+ str(String0) + \
+        #      '&air=0&acceleration=0&cleavage=0&incline=0&field1='+repr(String1)+'&field2='+repr(String2)+'&field3='+repr(String3)
+        url  = 'http://ec2-54-175-179-28.compute-1.amazonaws.com/update_general.php?site=FD01_RMO' + \
+                '&t=' + repr(timeStamp) + \
+                '&id='+ repr(String0) + \
+                '&d=' + repr(String1) + \
+                '&f1='+ repr(String2) + \
+                '&f2='+ repr(String3)  
         resp = urlopen(url).read()
-        #httpCode = resp.code
+        #hturtpCode = resp.code
         print('------------------------')
     except:
          print('We have an error!')
